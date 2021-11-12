513c260c494d7b8846f7cfd2c960d9cc1913b60b
sroxck <642261686@qq.com>
2021-11-12 14:59:01 +0800

    简洁性优化

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-<div id="app">
-  <router-view/>
-</div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
-
-<style>
-#app {
- 
-}
-</style>
